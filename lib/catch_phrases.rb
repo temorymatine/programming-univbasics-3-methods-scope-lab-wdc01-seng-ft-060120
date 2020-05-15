17dc4b0d8759e8fb531957dc7fb8551ef3c5ec55
Matine temory <temorymatine@gmail.com>
2020-05-15 19:55:29 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,11 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  
   phrase = "It's-a me, Mario!\n"
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!\n'
   puts status
 end
 
